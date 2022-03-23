d595af7053f59a4f288d33f80b3eeda8540f36ca
pstoebenau <pstoebenauwps@gmail.com>
2022-03-22 22:26:05 -0400

    Revert to flutter simple example

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,116 @@
-import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
-import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
 
-class DebugOptionsWidget extends StatefulWidget {
-  DebugOptionsWidget({Key key}) : super(key: key);
-  @override
-  _DebugOptionsWidgetState createState() => _DebugOptionsWidgetState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _DebugOptionsWidgetState extends State<DebugOptionsWidget> {
-  ARSessionManager arSessionManager;
-  ARObjectManager arObjectManager;
-  bool _showFeaturePoints = false;
-  bool _showPlanes = false;
-  bool _showWorldOrigin = false;
-  bool _showAnimatedGuide = true;
-  String _planeTexturePath = "Images/triangle.png";
-  bool _handleTaps = false;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  void dispose() {
-    super.dispose();
-    arSessionManager.dispose();
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Debug Options'),
-        ),
-        body: Container(
-            child: Stack(children: [
-          ARView(
-            onARViewCreated: onARViewCreated,
-            planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
-            showPlatformType: true,
-          ),
-          Align(
-            alignment: FractionalOffset.bottomRight,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              color: Color(0xFFFFFFF).withOpacity(0.5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SwitchListTile(
-                    title: const Text('Feature Points'),
-                    value: _showFeaturePoints,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _showFeaturePoints = value;
-                        updateSessionSettings();
-                      });
-                    },
-                  ),
-                  SwitchListTile(
-                    title: const Text('Planes'),
-                    value: _showPlanes,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _showPlanes = value;
-                        updateSessionSettings();
-                      });
-                    },
-                  ),
-                  SwitchListTile(
-                    title: const Text('World Origin'),
-                    value: _showWorldOrigin,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _showWorldOrigin = value;
-                        updateSessionSettings();
-                      });
-                    },
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-        ])));
-  }
-
-  void onARViewCreated(
-      ARSessionManager arSessionManager,
-      ARObjectManager arObjectManager,
-      ARAnchorManager arAnchorManager,
-      ARLocationManager arLocationManager) {
-    this.arSessionManager = arSessionManager;
-    this.arObjectManager = arObjectManager;
-
-    this.arSessionManager.onInitialize(
-          showFeaturePoints: _showFeaturePoints,
-          showPlanes: _showPlanes,
-          customPlaneTexturePath: _planeTexturePath,
-          showWorldOrigin: _showWorldOrigin,
-          showAnimatedGuide: _showAnimatedGuide,
-          handleTaps: _handleTaps,
-        );
-    this.arObjectManager.onInitialize();
-  }
-
-  void updateSessionSettings() {
-    this.arSessionManager.onInitialize(
-          showFeaturePoints: _showFeaturePoints,
-          showPlanes: _showPlanes,
-          customPlaneTexturePath: _planeTexturePath,
-          showWorldOrigin: _showWorldOrigin,
-        );
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
