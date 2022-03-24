1d9d514e1b32d03eac5bbbdf30676658316c2ce1
pstoebenau <pstoebenauwps@gmail.com>
2022-03-23 21:18:00 -0400

    Working udp server

--- lib/hello_world.dart
+++ lib/hello_world.dart
@@ -1,162 +1,89 @@
+import 'dart:ffi';
 import 'dart:math' as math;
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
-import 'package:vector_math/vector_math_64.dart' as vector;
+import 'package:vector_math/vector_math_64.dart';
+import 'package:udp/udp.dart';
 
 class HelloWorldPage extends StatefulWidget {
-  @override
-  _HelloWorldPagState createState() => _HelloWorldPagState();
+	@override
+	_HelloWorldPagState createState() => _HelloWorldPagState();
 }
 
 class _HelloWorldPagState extends State<HelloWorldPage> {
-  late ARKitController arkitController;
+	late ARKitController arkitController;
+	Vector3 position = Vector3(0, 0, 0);
+	Quaternion quaternion = Quaternion(0, 0, 0, 1);
+	// creates a UDP instance and binds it to the first available network
+	// interface on port 65000.
+	late UDP sender;
 
-  @override
-  void dispose() {
-    arkitController.dispose();
-    super.dispose();
-  }
+	void update(double time) async {
+		updateTransform();
+	}
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: const Text('ARKit in Flutter'),
-      ),
-      body: Container(
-        child: ARKitSceneView(
-          onARKitViewCreated: onARKitViewCreated,
-          environmentTexturing:
-              ARWorldTrackingConfigurationEnvironmentTexturing.automatic,
-        ),
-      ));
+	void updateTransform() async {
+		Matrix4? mat = await arkitController.pointOfViewTransform();
+		if (mat != null) {
+			position = mat.getTranslation();
+			quaternion = Quaternion.fromRotation(mat.getRotation());
+		}
 
-  void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
+		sendPacket();
+	}
 
-    this.arkitController.add(_createSphere());
-    this.arkitController.add(_createPlane());
-    this.arkitController.add(_createText());
-    this.arkitController.add(_createBox());
-    this.arkitController.add(_createCylinder());
-    this.arkitController.add(_createCone());
-    this.arkitController.add(_createPyramid());
-    this.arkitController.add(_createTube());
-    this.arkitController.add(_createTorus());
-    this.arkitController.add(_createCapsule());
-  }
+  // Specifically for use in Unreal Engine Live Link
+	void sendPacket() async {
+		String data = "${position.x*100} ${position.y*100} ${position.z*100} ${quaternion.x} ${quaternion.z} ${quaternion.y} ${quaternion.w}";
+    int dataLength = await sender.send(data.codeUnits, Endpoint.broadcast(port: Port(54321)));
+	}
 
-  ARKitNode _createSphere() => ARKitNode(
-        geometry:
-            ARKitSphere(materials: _createRandomColorMaterial(), radius: 0.04),
-        position: vector.Vector3(-0.1, -0.1, -0.5),
-      );
+	@override
+	void dispose() {
+		arkitController.dispose();
+		sender.close();
+		super.dispose();
+	}
 
-  ARKitNode _createPlane() {
-    final plane = ARKitPlane(
-      width: 1,
-      height: 1,
-      materials: [
-        ARKitMaterial(
-          transparency: 0.5,
-          diffuse: ARKitMaterialProperty.color(Colors.white),
-        )
-      ],
-    );
-    return ARKitNode(
-      geometry: plane,
-      position: vector.Vector3(0, 0, -1.5),
-    );
-  }
+	@override
+	Widget build(BuildContext context) => Scaffold(
+		appBar: AppBar(
+			title: const Text('ARKit in Flutter'),
+		),
+		body: Container(
+			child: ARKitSceneView(
+				onARKitViewCreated: onARKitViewCreated,
+				environmentTexturing:
+					ARWorldTrackingConfigurationEnvironmentTexturing.automatic,
+			),
+		),
+	);
 
-  ARKitNode _createText() {
-    final text = ARKitText(
-      text: 'Flutter',
-      extrusionDepth: 1,
-      materials: [
-        ARKitMaterial(
-          diffuse: ARKitMaterialProperty.color(Colors.blue),
-        )
-      ],
-    );
-    return ARKitNode(
-      geometry: text,
-      position: vector.Vector3(-0.3, 0.3, -1.4),
-      scale: vector.Vector3(0.02, 0.02, 0.02),
-    );
-  }
+	void onARKitViewCreated(ARKitController arkitController) async {
+		this.arkitController = arkitController;
+		sender = await UDP.bind(Endpoint.any(port: Port(5649)));
+		arkitController.updateAtTime = (time) => update(time);
 
-  ARKitNode _createBox() => ARKitNode(
-        geometry: ARKitBox(
-            width: 0.06,
-            height: 0.06,
-            length: 0.06,
-            chamferRadius: 0.01,
-            materials: _createRandomColorMaterial()),
-        position: vector.Vector3(-0.1, 0, -0.5),
-      );
+		this.arkitController.add(_createSphere());
+	}
 
-  ARKitNode _createCylinder() => ARKitNode(
-        geometry: ARKitCylinder(
-            radius: 0.05,
-            height: 0.09,
-            materials: _createRandomColorMaterial()),
-        position: vector.Vector3(-0.1, 0.1, -0.5),
-      );
+	ARKitNode _createSphere() => ARKitNode(
+		geometry:
+				ARKitSphere(materials: _createRandomColorMaterial(), radius: 0.04),
+		position: Vector3(0, 0, 0),
+	);
 
-  ARKitNode _createCone() => ARKitNode(
-        geometry: ARKitCone(
-            topRadius: 0,
-            bottomRadius: 0.05,
-            height: 0.09,
-            materials: _createRandomColorMaterial()),
-        position: vector.Vector3(0, -0.1, -0.5),
-      );
-
-  ARKitNode _createPyramid() => ARKitNode(
-        geometry: ARKitPyramid(
-            width: 0.09,
-            height: 0.09,
-            length: 0.09,
-            materials: _createRandomColorMaterial()),
-        position: vector.Vector3(0, -0.05, -0.5),
-      );
-
-  ARKitNode _createTube() => ARKitNode(
-        geometry: ARKitTube(
-            innerRadius: 0.03,
-            outerRadius: 0.05,
-            height: 0.09,
-            materials: _createRandomColorMaterial()),
-        position: vector.Vector3(0.1, 0.1, -0.5),
-      );
-
-  ARKitNode _createTorus() => ARKitNode(
-        geometry: ARKitTorus(
-            ringRadius: 0.04,
-            pipeRadius: 0.02,
-            materials: _createRandomColorMaterial()),
-        position: vector.Vector3(0.1, -0.1, -0.5),
-      );
-
-  ARKitNode _createCapsule() => ARKitNode(
-        geometry: ARKitCapsule(
-            capRadius: 0.02,
-            height: 0.06,
-            materials: _createRandomColorMaterial()),
-        position: vector.Vector3(0.1, 0, -0.5),
-      );
-
-  final _rnd = math.Random();
-  List<ARKitMaterial> _createRandomColorMaterial() {
-    return [
-      ARKitMaterial(
-        lightingModelName: ARKitLightingModel.physicallyBased,
-        metalness: ARKitMaterialProperty.value(_rnd.nextDouble()),
-        roughness: ARKitMaterialProperty.value(_rnd.nextDouble()),
-        diffuse: ARKitMaterialProperty.color(
-          Color((_rnd.nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0),
-        ),
-      )
-    ];
-  }
+	final _rnd = math.Random();
+	List<ARKitMaterial> _createRandomColorMaterial() {
+		return [
+			ARKitMaterial(
+				lightingModelName: ARKitLightingModel.physicallyBased,
+				metalness: ARKitMaterialProperty.value(_rnd.nextDouble()),
+				roughness: ARKitMaterialProperty.value(_rnd.nextDouble()),
+				diffuse: ARKitMaterialProperty.color(
+					Color((_rnd.nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0),
+				),
+			)
+		];
+	}
 }
